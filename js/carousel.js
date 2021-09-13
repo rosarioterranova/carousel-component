43627ada4faef17f656ef89167b679d9c76165ef
Rosario Terranova <rterranova@brydenwood.co.uk>
2021-09-13 10:18:07 +0200

    added sass support

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,21 @@
 export default class Carousel{
+
     constructor(option){
         this.option = option
+        this.render()
+    }
+
+    render(){
+        document.getElementById(this.option.container).innerHTML = `
+        <div class="carousel">
+            <div class="header">
+                <span class="material-icons">${this.option.icon}</span>
+                <div class="title">
+                    <p>${this.option.title}</p>
+                    <p>${this.option.subtitle}</p>
+                </div>
+            </div>
+        </div>
+        `
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import Carousel from "./carousel.js"
 
 const option1 = {
-    containter:"myCarousel1",
+    container:"myCarousel1",
     icon:"collections",
     title:"Fresh and just uploaded content",
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
@@ -17,7 +17,7 @@ const option1 = {
 }
 
 const option2 = {
-    containter:"myCarousel2",
+    container:"myCarousel2",
     icon:"event_seat",
     title:"Another carousel instance title",
     subtitle: "Consectetur adipiscing elit.",
